4ff18ac3ae21ea1d1321a717facab014c108fa04
SimtinicaLucian <simtinica.cosmin@gmail.com>
2021-03-23 16:18:29 +0200

    update - chart - save

--- src/app/services/api/incasari.service.ts
+++ src/app/services/api/incasari.service.ts
@@ -906,4 +906,40 @@ export class IncasariService {
   }
 
 
+     /**
+     * F
+     * M
+     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+     * @param reportProgress flag to report request and response progress.
+     */
+      public count(observe?: 'body', reportProgress?: boolean): Observable<any>;
+      public count(observe?: 'response', reportProgress?: boolean): Observable<HttpResponse<any>>;
+      public count(observe?: 'events', reportProgress?: boolean): Observable<HttpEvent<any>>;
+      public count(observe: any = 'body', reportProgress: boolean = false): Observable<any> {
+  
+          let headers = this.defaultHeaders;
+  
+          // to determine the Accept header
+          let httpHeaderAccepts: string[] = [
+              'application/json'
+          ];
+          const httpHeaderAcceptSelected: string | undefined = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+          if (httpHeaderAcceptSelected != undefined) {
+              headers = headers.set('Accept', httpHeaderAcceptSelected);
+          }
+          // to determine the Content-Type header
+          const consumes: string[] = [
+          ];
+  
+          return this.httpClient.get<any>(`${this.basePath}/incasari/countAll`,
+              {
+                  withCredentials: this.configuration.withCredentials,
+                  headers: headers,
+                  observe: observe,
+                  reportProgress: reportProgress
+              }
+          );
+      }
+
+
 }
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -5,7 +5,10 @@ import {
   ApexResponsive,
   ApexChart
 } from "ng-apexcharts";
-import {IncasariService} from '../services/api/incasari.service';
+import { IncasariService } from '../services/api/incasari.service';
+import { WeatherService } from '../services/api/weather.service';
+import { Chart } from 'chart.js';
+import { AdminService } from '../services/api/admin.service';
 
 export type ChartOptions = {
   series: ApexNonAxisChartSeries;
@@ -19,34 +22,101 @@ export type ChartOptions = {
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.scss']
 })
-export class TestComponent  {
+export class TestComponent {
+  // chart: any;
+  public rowses: any;
+  totalSum: any;
+  public rows: any;
+  public rowss: any;
+
   @ViewChild("chart") chart: ChartComponent;
   public chartOptions: Partial<ChartOptions>;
-  constructor(private incasariService : IncasariService) { 
+  constructor(private incasariService: IncasariService, private _weather: WeatherService, private adminService : AdminService) {}
 
-  this.chartOptions = {
-    series: [44, 55, 13, 43, 22],
-    chart: {
-      width: 380,
-      type: "pie"
-    },
-    labels: ["Team A", "Team B", "Team C", "Team D", "Team E"],
-    responsive: [
-      {
-        breakpoint: 480,
-        options: {
-          chart: {
-            width: 200
-          },
-          legend: {
-            position: "bottom"
+    ngOnInit() {
+
+  this.incasariService.count()
+    .subscribe((reses: any[]) => {
+      this.rowses = reses
+      console.log(this.rowses);
+
+      this.incasariService.searchTotal().subscribe((res : any) => {
+      this.rows = res;
+      console.log(this.rows);
+
+        this.incasariService.searchTotalTVA().subscribe((ress : any) => {
+          this.rowss = ress;
+          console.log(this.rowss)
+       
+      
+      
+      
+
+    this.chartOptions = {
+      series: [this.rowses, this.rows, this.rowss],
+      chart: {
+        width: 380,
+        type: "pie"
+      },
+      labels: ["Incasari", "Cheltuieli", "che"],
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: "bottom"
+            }
           }
         }
-      }
-    ]
-  };
-}
+      ]
+    };
+  })
+})
+})
+
+  // ngOnInit() {
+  //   this.incasariService.count()
+  //   .subscribe((reses: any[]) => {
+  //     this.rowses = reses
+  //     console.log(this.rowses);
+
+        
+
+  //       this.chart = new Chart('canvas', {
+  //         type: 'line',
+  //         data: {
+  //           labels: this.rowses,
+  //           datasets: [
+  //             {
+  //               data: this.rowses,
+  //               borderColor: '#3cba9f',
+  //               fill: false
+  //             },
+
+  //           ]
+  //         },
+  //         options: {
+  //           legend: {
+  //             display: false
+  //           },
+  //           scales: {
+  //             xAxes: [{
+  //               display: true
+  //             }],
+  //             yAxes: [{
+  //               display: true
+  //             }]
+  //           }
+  //         }
+  //       })
+
+  //     })
+  // }
 }
 
+}
 
 
